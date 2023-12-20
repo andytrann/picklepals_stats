3c4b233bbb62d351f58d5b2de2336bce85e5efcd
Andy Tran <andytran@utexas.edu>
2023-12-20 04:07:54 -0600

    Removed deactivate link from leader board and added formatting to the leaderboard and match history

--- test/integration/home_players_test.rb
+++ test/integration/home_players_test.rb
@@ -17,23 +17,6 @@ class HomePlayersTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', player_path(inactive_player), { count: 0, text: inactive_player.name.titleize }
   end
 
-  test "login as andy and check for deactivate links" do
-    log_in_as(@admin)
-    get root_path
-    assert_template 'static_pages/home'
-    active_players = Player.all.select(&:active)
-    first_page_of_players = active_players
-    first_page_of_players.each do |player|
-      assert_select 'a[href=?]', player_path(player), text: player.name.titleize
-      unless player == @admin
-        assert_select "form input[type='submit'][value='Deactivate']"
-      end
-    end
-    patch player_activation_path(:id => @non_admin.id, :email => @non_admin.email, :isActive => false)
-    @non_admin.reload
-    assert_not @non_admin.active?
-  end
-
   test "index as non-admin" do
     log_in_as(@non_admin)
     get root_path
